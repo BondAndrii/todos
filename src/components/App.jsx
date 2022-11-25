ce999d833cf971b98061e7ad37f1d4c1484a5145
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-11-25 23:59:24 +0200

    add todolist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      First day after blackout
-    </div>
-  );
-};
+import React, {Component} from "react";
+import TodoList from "./TodoList";
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 'id-1', text: 'Попрацювати над React', completed: false },
+      { id: 'id-2', text: 'Позайматись англійською', completed: false },
+      { id: 'id-3', text: 'Підтягнутись, качнуть прес', completed: false },
+      { id: 'id-4', text: 'Повчити планування', completed: false },
+      { id: 'id-5', text: "Поспілкуватись з сім'єю", completed: false },
+      { id: 'id-6', text: 'Пошук роботи', completed: false },
+      
+    ],
+  }
+  render() {
+    const { todos } = this.state;
+    return (
+      <div>
+        <h1>Список завдань</h1>
+          <TodoList todos={todos} />
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = ({ todos }) => 
+// со свойства props деструктуризируется свойство todos 
+(
+    <ul>
+        Todolist
+    </ul>
+)
+
+export default TodoList
